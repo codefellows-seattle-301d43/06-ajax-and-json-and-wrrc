796270071c9d0e09dced492c548c2b45aa697c3d
brinel321 <brian.c.nelson1@outlook.com>
2018-09-11 11:41:01 -0700

    finished all questions and code

--- brianandrew/scripts/article.js
+++ brianandrew/scripts/article.js
@@ -13,7 +13,7 @@ function Article (rawDataObj) {
 Article.all = [];
 
 // COMMENT: Why isn't this method written as an arrow function?
-// We col;d not write this as an arrow function because an arrow function removes our ability to properly use the contextual this.
+// We could not write this as an arrow function because an arrow function removes our ability to properly use the contextual this.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
@@ -21,7 +21,7 @@ Article.prototype.toHtml = function() {
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // This statement checks to see if the statement is true, if so, then the text after the ? is executed else the statement ater the : is executed.  In this case if the article is already published a published on date will be insterted, otherwise it'll open up the draft page.
+  // This statement checks to see if the statement is an if/else; if true, then the text after the ? is executed, else the statement ater the : is executed.  In this case if the article is already published a published on date will be insterted, otherwise it'll open up the draft page.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,7 +33,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// The loadAll function is called with in the fetchAll function as the fetchAll function is checking to see if the local storage contains rawData. During previous labs the rawData was not containted as a JSON string.
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -42,12 +42,23 @@ Article.loadAll = articleData => {
 
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
-  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  // REVIEW: What is this 'if' statement checking for? This is checking to see if the remote server data has already been stored to local storage, if true then data is pulled from the local storage, else it's pulled from the remote server.
+  //Where was the rawData set to local storage? We set rawData in the else statement w/in the success function.
   if (localStorage.rawData) {
-
-    Article.loadAll();
-
+    Article.loadAll(JSON.parse(rawData));
+    articleView.initIndexPage();
   } else {
-
+    $.ajax({
+      url: 'data/hackerIpsum.json',
+      method: 'GET',
+      success: (rawData) => {
+        console.log('response received');
+        localStorage.setItem('articlesStored', JSON.stringify(rawData));
+        Article.loadAll(rawData);
+        articleView.initIndexPage();
+      }
+    });
   }
 }
+
+
