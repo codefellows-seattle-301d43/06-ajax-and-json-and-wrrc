947e08d5f95fd577429bd23e5c1c5f8b969da9ed
Christina Gislason <mkcgislason@gmail.com>
2018-09-11 10:40:13 -0700

    added comments, trying to get ajax to work

---
+++ christina-kris/scripts/article.js
@@ -0,0 +1,69 @@
+'use strict';
+
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
+
+// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+Article.all = [];
+
+// COMMENT: Why isn't this method written as an arrow function?
+// This method is not written as an arrow function because it uses "this".
+Article.prototype.toHtml = function() {
+  let template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // Not sure? Check the docs!
+  // The code below is a conditional ternary. It is assigning the publishStatus of each article. If the article has been published, it will grab the info about the number of days ago the article was published. Otherwise, it will use a default draft value. 
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+// REVIEW: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
+
+// REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
+
+// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// This function is called at the bottom, within the fetchAll method. 'rawData' now represents a parameter, which is the data in local storage. This is different from previous labs, when the data lived on a file located in a folder with the code versus local storage, which is separate from the code.
+Article.loadAll = articleData => {
+  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+
+  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+}
+
+// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+Article.fetchAll = () => {
+  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  console.log('request sent');
+  if (localStorage.rawData) {
+
+    Article.loadAll(rawData);
+
+  } else {
+    $.ajax({
+      url: './data/hackerIpsum.json',
+      method: 'GET',
+      success: (articleData) => { //in JSON file
+        //debugger;
+        console.log(articleData);
+        articleData.forEach( (value, i) => {
+          Article.loadAll(value[i])
+        });
+      } 
+    });
+  }
+
+  Article.all.forEach(value => {
+    value.toHtml();
+  });
+    
+}
